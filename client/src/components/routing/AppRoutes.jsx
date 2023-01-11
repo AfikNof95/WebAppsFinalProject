e3c15be15a14bce408fd3fed0adc76c74ed751dc
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2023-01-11 19:33:17 +0200

    Just the beggining

--- client/src/components/routing/AppRoutes.jsx
+++ client/src/components/routing/AppRoutes.jsx
@@ -1,3 +1,4 @@
+import Checkout from "../Checkout/Checkout";
 import CartPage from "../../pages/Cart/Cart";
 import Homepage from "../Homepage/Homepage";
 import NotFound from "../NotFound/NotFound";
@@ -11,6 +12,10 @@ const appRoutes = [
   {
     path: "/login",
     element: <div style={{ paddingTop: "100px" }}>To Do login</div>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
   },
   {
     path: "/cart",
