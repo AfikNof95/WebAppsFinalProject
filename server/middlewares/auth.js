e07db9a4522e751a3cc909f53da0660fd30eac35
AfikNof95 <afiknof9@gmail.com>
2023-01-24 22:10:20 +0200

    Server

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,23 +1,23 @@
-const admin = require("firebase-admin");
-const fireBaseConfig = require("./firebaseAdminConfig.json");
+// const admin = require("firebase-admin");
+// const fireBaseConfig = require("./firebaseAdminConfig.json");
 
-admin.initializeApp(fireBaseConfig);
+// admin.initializeApp(fireBaseConfig);
 
-const isAuthorized = async (res, req, next) => {
-  const authHeader = req.headers.authorization;
+// const isAuthorized = async (res, req, next) => {
+//   const authHeader = req.headers.authorization;
 
-  if (authHeader) {
-    try {
-      const token = authHeader.split(" ")[1];
-      await admin.auth().verifyIdToken(token);
-      return next();
-    } catch (ex) {
-      console.error(ex.message);
-      return res.status(403).send("Unauthorized");
-    }
-  } else {
-    res.status(401).send("Unauthorized");
-  }
-};
+//   if (authHeader) {
+//     try {
+//       const token = authHeader.split(" ")[1];
+//       await admin.auth().verifyIdToken(token);
+//       return next();
+//     } catch (ex) {
+//       console.error(ex.message);
+//       return res.status(403).send("Unauthorized");
+//     }
+//   } else {
+//     res.status(401).send("Unauthorized");
+//   }
+// };
 
-module.exports = { isAuthorized };
+// module.exports = { isAuthorized };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,12 @@ const AddressRoutes = require("./address.routes");
 const CartRoutes = require("./cart.routes");
 const ProductRoutes = require("./product.routes");
 const OrderRoutes = require("./order.routes");
-const { isAuthorized } = require("../middlewares/auth");
+// const { isAuthorized } = require("../middlewares/auth");
 
 router.use("/User", UserRoutes);
 router.use("/Address", AddressRoutes);
 router.use("/Category", CategoryRoutes);
-router.use("/Cart", isAuthorized, CartRoutes);
+router.use("/Cart" /*, isAuthorized*/, CartRoutes);
 router.use("/Product", ProductRoutes);
 router.use("/Order", OrderRoutes);
 
